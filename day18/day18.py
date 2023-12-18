26e86ff795a27b67bbbfa948b615fbc9c7e9a686
Emil Lynegaard <ecly@mailbox.org>
2023-12-18 22:41:06 +0100

    start part2

--- day18/day18.py
+++ day18/day18.py
@@ -4,23 +4,7 @@ from collections import deque
 directions = {"R": 1, "L": -1, "U": -1j, "D": 1j}
 
 
-def print_grid(grid):
-    max_x = int(max(p.real for p in grid))
-    max_y = int(max(p.imag for p in grid))
-    print("_" * 10)
-    for y in range(max_y + 1):
-        for x in range(max_x + 1):
-            if x + y * 1j in grid:
-                c = "#"
-            else:
-                c = "."
-            print(c, end="")
-        print()
-
-    print("-" * 10)
-
-
-def parse(filename):
+def parse_part1(filename):
     grid = {}
     pos = 0j
     for line in open(filename):
@@ -30,12 +14,13 @@ def parse(filename):
             pos += delta
             grid[pos] = color
 
-    print_grid(grid)
     return grid
 
 
-def part1(grid):
-    start = 1+1j
+def part1(filename):
+    grid = parse_part1(filename)
+
+    start = 1 + 1j
     queue = deque([start])
     seen = set()
     while queue:
@@ -48,15 +33,27 @@ def part1(grid):
             queue.append(new_pos)
     return len(grid) + len(seen)
 
+def parse_part2(filename):
+    points = []
+    pos = 0j
+
 def part2(grid):
+    for line in open(filename):
+        direction, amount, color = line.strip().split()
+        delta = directions.get(direction)
+        for _ in range(int(amount)):
+            pos += delta
+            grid[pos] = color
+
+    return grid
+
     pass
 
 
 def main():
     filename = "test" if len(sys.argv) == 1 else sys.argv[1]
-    grid = parse(filename)
-    print(part1(grid))
-    print(part2(grid))
+    print(part1(filename))
+    print(part2(filename))
 
 
 if __name__ == "__main__":
